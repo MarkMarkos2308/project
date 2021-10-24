776604d254710c8e1581a276b43fd9819dce3799
Trust-Nobody <hovo.baghdasaryan.03@mail.ru>
2021-10-24 23:52:19 +0400

    Project-Hovhannes

---
+++ Project/register/urls.py
@@ -0,0 +1,27 @@
+from . import views
+from django.urls import path
+
+# urlpatterns = [
+#     path('about/', views.home, name='register-home')
+# ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Project/register/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+
+def home(request):
+    return render(request, 'register/index.html')
+
+
+
+
+
+
